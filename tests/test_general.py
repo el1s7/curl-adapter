c937f992fef191e130e7c8a03d6b5d3e3a5ac080
el1s7 <github@elis.cc>
2025-05-01 17:46:23 +0200

    change httpbin.org to httpbingo.org, better server

--- tests/test_general.py
+++ tests/test_general.py
@@ -8,6 +8,8 @@ import requests.adapters
 from curl_adapter import CurlCffiAdapter, PyCurlAdapter, CurlInfo
 
 
+test_server = "https://httpbingo.org" #httpbin.org, httpbingo.org, postman-echo.com
+
 @pytest.mark.parametrize("curl_adapter", [CurlCffiAdapter, PyCurlAdapter])
 class TestFunctions:
 
@@ -16,7 +18,7 @@ class TestFunctions:
 		with requests.Session() as s:
 			s.mount("http://", curl_adapter(use_curl_content_decoding=True))
 			s.mount("https://", curl_adapter(use_curl_content_decoding=True))
-			r = s.get("https://httpbin.org/gzip")
+			r = s.get(f"{test_server}/gzip")
 			assert r.status_code == 200
 			assert r.json()["gzipped"]
 
@@ -25,7 +27,7 @@ class TestFunctions:
 		with requests.Session() as s:
 			s.mount("http://", curl_adapter(use_curl_content_decoding=False))
 			s.mount("https://", curl_adapter(use_curl_content_decoding=False))
-			r = s.get("https://httpbin.org/gzip")
+			r = s.get(f"{test_server}/gzip")
 			assert r.status_code == 200
 			assert r.json()["gzipped"]
 
@@ -38,32 +40,36 @@ class TestFunctions:
 		with requests.Session() as s:
 			s.mount("http://", curl_adapter(use_curl_content_decoding=True))
 			s.mount("https://", curl_adapter(use_curl_content_decoding=True))
-			r = s.get("https://httpbin.org/brotli")
+			r = s.get(f"{test_server}/get", headers={
+				"Accept-Encoding": "br"
+			})
 			assert r.status_code == 200
-			assert r.json()["brotli"]
+			assert "headers" in r.json()
 
 	def test_brotli_python(self, curl_adapter):
 
 		with requests.Session() as s:
 			s.mount("http://", curl_adapter(use_curl_content_decoding=False))
 			s.mount("https://", curl_adapter(use_curl_content_decoding=False))
-			r = s.get("https://httpbin.org/brotli")
+			r = s.get(f"{test_server}/get", headers={
+				"Accept-Encoding": "br"
+			})
 			assert r.status_code == 200
-			assert r.json()["brotli"]
+			assert "headers" in r.json()
 
 	def test_raw_response(self, curl_adapter):
 		with requests.Session() as s:
 			s.mount("http://", curl_adapter(use_curl_content_decoding=True))
 			s.mount("https://", curl_adapter(use_curl_content_decoding=True))
 
-			r  = s.get("https://httpbin.org/gzip")
+			r  = s.get(f"{test_server}/gzip")
 
 			size1 = r.raw.tell()
 
 			s.mount("http://", curl_adapter(use_curl_content_decoding=False))
 			s.mount("https://", curl_adapter(use_curl_content_decoding=False))
 
-			r  = s.get("https://httpbin.org/gzip")
+			r  = s.get(f"{test_server}/gzip")
 
 			size2 = r.raw.tell()
 
@@ -73,41 +79,43 @@ class TestFunctions:
 		with requests.Session() as s:
 			s.mount("http://", curl_adapter())
 			s.mount("https://", curl_adapter())
-			r = s.get("https://httpbin.org/absolute-redirect/3")
+			r = s.get(f"{test_server}/absolute-redirect/3")
 			assert r.status_code == 200
 
 	def test_relative_redirects(self, curl_adapter):
 		with requests.Session() as s:
 			s.mount("http://", curl_adapter())
 			s.mount("https://", curl_adapter())
-			r = s.get("https://httpbin.org/relative-redirect/3")
+			r = s.get(f"{test_server}/relative-redirect/3")
 			assert r.status_code == 200
 
 	def test_fingerprints(self, curl_adapter):
 		with requests.Session() as s:
 			s.mount("http://", curl_adapter())
 			s.mount("https://", curl_adapter())
-			curl_request = s.get("https://tls.browserleaks.com/json")
+			curl_request = s.get("https://tools.scrapfly.io/api/fp/ja3")
 
 			s.mount("http://", requests.adapters.HTTPAdapter())
 			s.mount("https://", requests.adapters.HTTPAdapter())
 
-			normal_request = s.get("https://tls.browserleaks.com/json")
+			normal_request = s.get("https://tools.scrapfly.io/api/fp/ja3")
 
-			assert curl_request.json()["ja3_hash"] != normal_request.json()["ja3_hash"]
+			assert curl_request.json()["ja3n_digest"] != normal_request.json()["ja3n_digest"]
 
 	def test_cookies(self, curl_adapter):
 		with requests.Session() as s:
 			s.mount("http://", curl_adapter())
 			s.mount("https://", curl_adapter())
 
-			s.get("https://httpbin.org/cookies/set?cookie1=one", allow_redirects=False)
-			s.get("https://httpbin.org/cookies/set?cookie2=two", allow_redirects=False)
-			res = s.get("https://httpbin.org/cookies/set?cookie3=three")
+			s.get(f"{test_server}/cookies/set?cookie1=one", allow_redirects=False)
+			s.get(f"{test_server}/cookies/set?cookie2=two", allow_redirects=False)
+			res = s.get(f"{test_server}/cookies/set?cookie3=three")
 
-			assert res.json()["cookies"]["cookie1"] == "one"
-			assert res.json()["cookies"]["cookie2"] == "two"
-			assert res.json()["cookies"]["cookie3"] == "three"
+			res_cookies = res.json()["cookies"] if "cookies" in res.json() else res.json()
+
+			assert res_cookies["cookie1"] == "one"
+			assert res_cookies["cookie2"] == "two"
+			assert res_cookies["cookie3"] == "three"
 
 			assert s.cookies.get("cookie1") == "one"
 			assert s.cookies.get("cookie2") == "two"
@@ -117,7 +125,7 @@ class TestFunctions:
 		with requests.Session() as s:
 			s.mount("http://", curl_adapter())
 			s.mount("https://", curl_adapter())
-			r = s.get("https://httpbin.org/get")
+			r = s.get(f"{test_server}/get")
 			curl_info: CurlInfo = r.get_curl_info()
 			assert "local_ip" in curl_info
 
@@ -126,7 +134,7 @@ class TestFunctions:
 			with requests.Session() as s:
 				s.mount("http://", curl_adapter())
 				s.mount("https://", curl_adapter())
-				s.get("https://httpbin.org/delay/10", timeout=2)
+				s.get(f"{test_server}/delay/10", timeout=2)
 		assert "Operation too slow." in str(err.value)
 
 	def test_ssl_verify(self, curl_adapter):
